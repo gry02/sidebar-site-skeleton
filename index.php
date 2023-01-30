24c3996dbbedd75d706a0218c802d9d8be0c3bdf
Selçuk Giray <80980937+girayselcuk02@users.noreply.github.com>
2023-01-30 15:24:02 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,154 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>sidebar site skeleton</title>
+    <link rel="stylesheet" href="css/main.css">
+    <link rel="stylesheet" href="css/fontawesome.min.css">
+    <script type="text/javascript" src="//code.jquery.com/jquery-3.6.0.min.js"></script>
+    <link href="https://cdn.jsdelivr.net/npm/summernote@0.8.18/dist/summernote-lite.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/summernote@0.8.18/dist/summernote-lite.min.js"></script>
+    <script src="js/main.js"></script>
+</head>
+<body>
+    
+<header class="header">header</header>
+<main class="main">
+<aside class="sidebar">
+   <p>sdsdsdd</p>
+   <p>sdsdsdd</p>
+   <p>sdsdsdd</p>
+   <p>sdsdsdd</p>
+   <p>sdsdsdd</p>
+   <p>sdsdsdd</p>
+   <p>sdsdsdd</p>
+   <p>sdsdsdd</p>
+   <p>sdsdsdd</p>
+   <p>sdsdsdd</p>
+   <p>sdsdsdd</p>
+   <p>sdsdsdd</p>
+   <p>sdsdsdd</p>
+   <p>sdsdsdd</p>
+   <p>sdsdsdd</p>
+   <p>sdsdsdd</p>
+   <p>sdsdsdd</p>
+   <p>sdsdsdd</p>
+   <p>sdsdsdd</p>
+   <p>sdsdsdd</p>
+   <p>sdsdsdd</p>
+   <p>sdsdsdd</p>
+   <p>sdsdsdd</p>
+   <p>sdsdsdd</p>
+   <p>sdsdsdd</p>
+   <p>sdsdsdd</p>
+   <p>sdsdsdd</p>
+   <p>sdsdsdd</p>
+   <p>sdsdsdd</p>
+   <p>sdsdsdd</p>
+   <p>sdsdsdd</p>
+   <p>sdsdsdd</p>
+   <p>sdsdsdd</p>
+   <p>sdsdsdd</p>
+   <p>sdsdsdd</p>
+   <p>sdsdsdd</p>
+   <p>sdsdsdd</p>
+   <p>sdsdsdd</p>
+   <p>sdsdsdd</p>
+   <p>sdsdsdd</p>
+   <p>sdsdsdd</p>
+   <p>sdsdsdd</p>
+   <p>sdsdsdd</p>
+   <p>sdsdsdd</p>
+   <p>sdsdsdd</p>
+   <p>sdsdsdd</p>
+   <p>sdsdsdd</p>
+   <p>sdsdsdd</p>
+   <p>sdsdsdd</p>
+   <p>sdsdsdd</p>
+   <p>sdsdsdd</p>
+
+</aside>
+
+<div class="content">4
+    <p>ds</p>
+    <p>ds</p>
+    <p>ds</p>
+    <p>ds</p>
+    <p>ds</p>
+    <p>ds</p>
+    <p>ds</p>
+    <p>ds</p>
+    <p>ds</p>
+    <p>ds</p>
+    <p>ds</p>
+    <p>ds</p>
+    <p>ds</p>
+    <p>ds</p>
+    <p>ds</p>
+    <p>ds</p>
+    <p>ds</p>
+    <p>ds</p>
+    <p>ds</p>
+    <p>ds</p>
+    <p>ds</p>
+    <p>ds</p>
+    <p>ds</p>
+    <p>ds</p>
+    <p>ds</p>
+    <p>ds</p>
+    <p>ds</p>
+    <p>ds</p>
+    <p>ds</p>
+    <p>ds</p>
+    <p>ds</p>
+    <p>ds</p>
+    <p>ds</p>
+    <p>ds</p>
+    <p>ds</p>
+    <p>ds</p>
+    <p>ds</p>
+    <p>ds</p>
+    <p>ds</p>
+    <p>ds</p>
+    <p>ds</p>
+    <p>ds</p>
+    <p>ds</p>
+    <p>ds</p>
+    <p>ds</p>
+    <p>ds</p>
+    <p>ds</p>
+    <p>ds</p>
+    <p>ds</p>
+    <p>ds</p>
+    <p>ds</p>
+    <p>ds</p>
+    <p>ds</p>
+    <p>ds</p>
+    <p>ds</p>
+    <p>ds</p>
+    <p>ds</p>
+    <p>ds</p>
+    <p>ds</p>
+    <p>ds</p>
+    <p>ds</p>
+    <p>ds</p>
+    <p>ds</p>
+    <p>ds</p>
+    <p>ds</p>
+    <p>ds</p>
+    <p>ds</p>
+    <p>ds</p>
+    <p>ds</p>
+    <p>ds</p>
+    <p>ds</p>
+    <p>ds</p>
+    <p>ds</p>
+</div>
+
+</main>
+
+<script src="https://kit.fontawesome.com/d42ed577d8.js" crossorigin="anonymous"></script>
+</body>
+</html>
